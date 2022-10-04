7c9c940aba2da47365b43182d3fd3aa5bb323394
Jaycn <okonkwojude69@gmail.com>
2022-10-04 10:57:03 +0100

    done

--- src/App.js
+++ src/App.js
@@ -7,27 +7,23 @@ import big from "./images/big_circle.png";
 import purple from "./images/purple_circle.png";
 import spiral from "./images/spiral.png";
 import yellow from "./images/yellow_circle.png";
-import AppContext from "./context/context";
-import React, { useContext } from "react";
+import React from "react";
 function App() {
-  const { phoneMedia } = useContext(AppContext);
   return (
-    <div className="ent">
-      {!phoneMedia && (
-        <>
-          <img src={circle} alt="pic here" className="mg1" />
-          <img src={half} alt="pic here" className="mg2" />
-          <img src={big} alt="pic here" className="mg3" />
-          <img src={purple} alt="pic here" className="mg4" />
-          <img src={spiral} alt="pic here" className="mg5" />
-          <img src={yellow} alt="pic here" className="mg6" />
-        </>
-      )}
+    <>
+      <div className="ent">
+        <img src={circle} alt="pic here" className="mg1" />
+        <img src={half} alt="pic here" className="mg2" />
+        <img src={big} alt="pic here" className="mg3" />
+        <img src={purple} alt="pic here" className="mg4" />
+        <img src={spiral} alt="pic here" className="mg5" />
+        <img src={yellow} alt="pic here" className="mg6" />
+      </div>
       <div className="box">
         <Form />
         <Logo />
       </div>
-    </div>
+    </>
   );
 }
 
